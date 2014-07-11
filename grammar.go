62b311a1fe541d6d176a11f83aa6a4ad6ad2f0bd
Ghislain Gt <gt.ghislain@gmail.com>
2014-07-11 20:31:25 +0100

    add grammar file
    - contain all grammar (future) package

---
+++ grammar.go
@@ -0,0 +1,56 @@
+package main
+
+import "bytes"
+
+type Token string
+
+type NonTerminal struct {
+	Left  Token
+	Right Token
+}
+
+type Terminal string
+
+type Rule interface{}
+
+type Items []Token
+
+func (items *Items) String() string {
+	var buf bytes.Buffer
+
+	buf.WriteString("{ ")
+	for _, i := range *items {
+		buf.WriteString(string(i) + " ")
+	}
+	buf.WriteString("}")
+
+	return buf.String()
+}
+
+type Rules []Rule
+
+type Grammar map[Token]Rules
+
+func (g Grammar) GetListOfT(t Token) []*Terminal {
+	nt := []*Terminal{}
+
+	for _, i := range g[t] {
+		v, ok := i.(Terminal)
+		if ok {
+			nt = append(nt, &v)
+		}
+	}
+	return nt
+}
+
+func (g Grammar) GetListOfNT(t Token) []*NonTerminal {
+	nt := []*NonTerminal{}
+
+	for _, i := range g[t] {
+		v, ok := i.(NonTerminal)
+		if ok {
+			nt = append(nt, &v)
+		}
+	}
+	return nt
+}
