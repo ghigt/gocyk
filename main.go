2f224a52e43c5a50e14fc8042114e429a9858039
Ghislain Gt <gt.ghislain@gmail.com>
2014-07-16 11:15:18 +0100

    add a progression view of the table
    - require the ‘github.com/ghigt/gotd/term' package
    - refresh every 500ms to be able to view
    - activated with the -v flag

--- main.go
+++ main.go
@@ -2,22 +2,34 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"os"
 	"strings"
+	"time"
 
 	"github.com/ghigt/gocyk/table"
+	"github.com/ghigt/gotd/term"
 )
+
+var verbose = flag.Bool("v", false, "Print recognition table")
 
 func main() {
 	var input string
 	rtable := table.New(&grammar)
 
-	if len(os.Args) >= 2 {
-		input = os.Args[1]
+	flag.Parse()
+
+	if len(flag.Args()) > 0 {
+		input = flag.Args()[0]
 	} else {
 		fmt.Println("Please, precise an input")
 		return
+	}
+
+	// init term
+	if err := term.TGetEnt(); err != nil {
+		fmt.Println(err)
 	}
 
 	scanner := bufio.NewScanner(strings.NewReader(input))
@@ -25,12 +37,18 @@ func main() {
 
 	for i := 0; scanner.Scan(); i++ {
 		rtable.Add(scanner.Text())
+		if *verbose {
+			// clear screen
+			if err := term.SetCap("cl"); err != nil {
+				fmt.Println(err)
+			}
+			table.PrettyPrint(rtable)
+			time.Sleep(500 * time.Millisecond)
+		}
 	}
 	if err := scanner.Err(); err != nil {
 		fmt.Fprintln(os.Stderr, "reading input:", err)
 	}
-
-	table.PrettyPrint(rtable)
 
 	if rtable.Valid() {
 		fmt.Println("It works :)")
