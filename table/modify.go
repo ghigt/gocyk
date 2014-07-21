413ca1603214013c0f6fc020384e9a7538026a06
Ghislain Gt <gt.ghislain@gmail.com>
2014-07-21 19:52:46 +0100

    add comments to table/modify.go

--- table/modify.go
+++ table/modify.go
@@ -2,12 +2,16 @@ package table
 
 import "errors"
 
+// Add adds a new column at the end of the recognition table and
+// fills it with the appropriate items.
 func (rt *RTable) Add(s string) {
 	c := new(Column)
 	*rt = append(*rt, c)
 	c.AddAndCompute(s, rt)
 }
 
+// insertFollowing computes the rest each item until the end and
+// modify it appropriately. It also modifies only from a given position.
 func (rt *RTable) insertFollowing(pos int) {
 	for col := pos + 1; col < len(*rt); col++ {
 		(*rt)[col].Front(&Item{})
@@ -15,6 +19,8 @@ func (rt *RTable) insertFollowing(pos int) {
 	}
 }
 
+// Insert inserts a new column at give position of the
+// recognition table and fills it with the appropriate items.
 func (rt *RTable) Insert(s string, pos int) error {
 	if pos < 0 || pos > len(*rt) {
 		return errors.New("index out of range")
@@ -34,6 +40,8 @@ func (rt *RTable) Insert(s string, pos int) error {
 	return nil
 }
 
+// removeFollowing computes the rest each item until the end and
+// modify it appropriately. It also modifies only from a given position.
 func (rt *RTable) removeFollowing(pos int) {
 	for col := pos; col < len(*rt); col++ {
 		(*rt)[col].PopFront()
@@ -41,6 +49,8 @@ func (rt *RTable) removeFollowing(pos int) {
 	}
 }
 
+// Remove removes a column at give position of the
+// recognition table and compute the new items appropriately.
 func (rt *RTable) Remove(pos int) error {
 	if pos < 0 || pos >= len(*rt) {
 		return errors.New("index out of range")
