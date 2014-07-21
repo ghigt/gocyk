b35d7e742b8863fb66cca2148984eccb02663c48
Ghislain Gt <gt.ghislain@gmail.com>
2014-07-21 20:03:35 +0100

    add comments to table/table.go

--- table/table.go
+++ table/table.go
@@ -6,27 +6,34 @@ import (
 	gram "github.com/ghigt/gocyk/grammar"
 )
 
-// RTable is the type to build a recognition table
+// RTable is the type to build a recognition table.
 type RTable []*Column
 
+// grammar is the variable used in the grammar package.
 var grammar *gram.Grammar
 
+// New returns a new recognition table for the given grammar.
 func New(g *gram.Grammar) *RTable {
 	grammar = g
 
 	return &RTable{}
 }
 
-func (rt *RTable) GetColumn(index int) *Column {
-	return (*rt)[index]
+// GetColumn returns the column corresponding to the given parameter.
+func (rt *RTable) GetColumn(col int) *Column {
+	return (*rt)[col]
 }
 
+// GetItem returns item corresponding to the column and index given
+// in parameter.
 func (rt *RTable) GetItem(column, index int) *Item {
 	c := (*rt).GetColumn(column)
 
 	return c.GetItem(index)
 }
 
+// Valid returns true if the recognition table is valid. This method
+// checks if the top left item is empty or not.
 func (rt *RTable) Valid() bool {
 	if rt.GetItem(len(*rt)-1, 0).Empty() != true {
 		return true
@@ -34,6 +41,8 @@ func (rt *RTable) Valid() bool {
 	return false
 }
 
+// ValidFor returns true if the recognition table is valid for a given
+// range. This method checks if the top left item is empty or not.
 func (rt *RTable) ValidFor(beg, end int) bool {
 	length := len(*rt)
 
