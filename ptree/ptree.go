6664a845367f92b921f0b874b87d1dd289f6b292
Ghislain Gt <gt.ghislain@gmail.com>
2014-07-24 18:16:56 +0100

    package tree called now ptree
    For “Parsing Tree” in short

---
+++ ptree/ptree.go
@@ -0,0 +1,21 @@
+package ptree
+
+import "github.com/ghigt/gocyk/grammar"
+
+type PTree struct {
+	Right *PTree
+	Value grammar.Token
+	Left  *PTree
+}
+
+func (t *PTree) InsertRight(v grammar.Token) {
+	t.Right = New(v)
+}
+
+func (t *PTree) InsertLeft(v grammar.Token) {
+	t.Left = New(v)
+}
+
+func New(v grammar.Token) *PTree {
+	return &Tree{nil, v, nil}
+}
