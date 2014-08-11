9132e6e34b40b28e3329f68271f732a1596ef884
Ghislain Guiot <gt.ghislain@gmail.com>
2014-08-11 18:19:33 +0100

    modifications on test.sh

--- examples/test.sh
+++ examples/test.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-go build -o examples ex_go_gram.go go_gram.go;
-        (./examples "`cat test/test0.go.g`" > log  || echo test0 failed) &&
-        (./examples "`cat test/test1.go.g`" >> log || echo test1 failed) &&
-        (./examples "`cat test/test2.go.g`" >> log || echo test2 failed) &&
-        (./examples "`cat test/test3.go.g`" >> log || echo test3 failed) &&
-        (./examples "`cat test/test4.go.g`" >> log || echo test4 failed) &&
-        (./examples "`cat test/test5.go.g`" >> log || echo test4 failed) &&
-        (./examples "`cat test/test.go.g`"  >> log || echo test. failed) &&
+go build -o examples ex_go_gram.go go_gram.go && (
+        (./examples "`cat test/test0.go.g`" >  log && echo test0 PASS || echo test0 failed) &&
+        (./examples "`cat test/test1.go.g`" >> log && echo test1 PASS || echo test1 failed) &&
+        (./examples "`cat test/test2.go.g`" >> log && echo test2 PASS || echo test2 failed) &&
+        (./examples "`cat test/test3.go.g`" >> log && echo test3 PASS || echo test3 failed) &&
+        (./examples "`cat test/test4.go.g`" >> log && echo test4 PASS || echo test4 failed) &&
+        (./examples "`cat test/test5.go.g`" >> log && echo test4 PASS || echo test4 failed) &&
+        (./examples "`cat test/test.go.g`"  >> log && echo test. PASS || echo test. failed) &&
 
-        (./examples "`cat test/test`"       >> log || echo test failed);
+        (./examples "`cat test/test`"       >> log && echo test  PASS || echo test  failed) 
+)
