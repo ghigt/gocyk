4e3878f70c8216e3e88b4248fdeabdbdd0fcd898
Ghislain Guiot <gt.ghislain@gmail.com>
2014-08-13 22:01:24 +0100

    small changes on tests

--- examples/test.sh
+++ examples/test.sh
@@ -6,7 +6,7 @@ go build -o examples ex_go_gram.go go_gram.go && (
         (./examples "`cat test/test2.go.g`" >> log && echo test2 PASS || echo test2 failed) &&
         (./examples "`cat test/test3.go.g`" >> log && echo test3 PASS || echo test3 failed) &&
         (./examples "`cat test/test4.go.g`" >> log && echo test4 PASS || echo test4 failed) &&
-        (./examples "`cat test/test5.go.g`" >> log && echo test4 PASS || echo test4 failed) &&
+        (./examples "`cat test/test5.go.g`" >> log && echo test5 PASS || echo test5 failed) &&
         (./examples "`cat test/test.go.g`"  >> log && echo test. PASS || echo test. failed) &&
 
         (./examples "`cat test/test`"       >> log && echo test  PASS || echo test  failed) 
